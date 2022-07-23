2c48e4cd1e518a6f5115b6022ff653ff4c9fbece
124c4a11 <124c4a11@mail.ru>
2022-07-23 21:15:30 +0300

    Improved animation working

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
-const accordions = document.querySelectorAll('.accordion');
+class Accordion {
+  constructor(accordion) {
+    this.isAnimate = false;
 
-if (accordions.length) {
-  for (const accordion of accordions) {
-    accordion.addEventListener('click', function(e) {
+    accordion.addEventListener('click', this.clickHandler);
+  }
+
+  clickHandler = (e) => {
       e.stopPropagation();
+
+      if (this.isAnimate) return;
 
       const btn = e.target.closest('.accordion__btn');
 
@@ -19,15 +24,37 @@ if (accordions.length) {
         btn.setAttribute('aria-expanded', 'true');
       }
 
+      const delay = parseFloat(getComputedStyle(content).transitionDuration) * 1000 || 0;
+
       if (content.style.height) {
+        this.isAnimate = true;
+
         content.style.height = `${content.scrollHeight}px`;
 
-        setTimeout(() => content.style.height = '', 1);
+        setTimeout(() => { content.style.height = ''; }, 0);
+        setTimeout(() => this.isAnimate = false, delay);
       } else {
+        this.isAnimate = true;
+
         content.style.height = `${content.scrollHeight}px`;
 
-        setTimeout(() => content.style.height = 'auto', 200);
+        if (delay) {
+          setTimeout(() => {
+            content.style.height = 'auto';
+
+            this.isAnimate = false;
+         }, delay);
+        } else {
+          content.style.height = 'auto';
+        }
       }
-    });
+  }
+}
+
+const accordions = document.querySelectorAll('.accordion');
+
+if (accordions.length) {
+  for (const accordion of accordions) {
+    new Accordion(accordion);
   }
 }
